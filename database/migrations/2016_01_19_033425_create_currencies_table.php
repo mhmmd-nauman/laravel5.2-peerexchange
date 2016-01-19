991b6e121868913e090a3ce1aeb715af095699fe
Marco Bassaletti <bassaletti@gmail.com>
2016-01-19 00:48:49 -0300

    Created currency model and added migration to fill initial currencies.

---
+++ database/migrations/2016_01_19_033425_create_currencies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function(Blueprint $table) {
+            $table->string('code', 3);
+            $table->string('name');
+            $table->timestamps();
+
+            $table->primary('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('currencies');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CurrencyTableSeeder::class);
         $this->call(UserRoleTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(TransactionTypeTableSeeder::class);
